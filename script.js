f22e59ec34057c086b1115487a3215260896a3db
Mihail <mihailmelinte74@gmail.com>
2024-09-02 15:06:47 +0300

    Add functionality and styling

--- script.js
+++ script.js
@@ -3,27 +3,22 @@ const password = document.querySelector("#pass");
 const confirmPassword = document.querySelector("#cpass");
 const formButton = document.querySelector(".form-button");
 
-confirmPassword.addEventListener("input", () => {
+confirmPassword.addEventListener("blur", () => {
     if(password.value !== confirmPassword.value) {
         passwordCheckPara.classList.remove("hidden");
         passwordCheckPara.classList.add("show");
         formButton.classList.add("disabled");
         formButton.disabled = true;
+
+        password.classList.add("invalid");
+        confirmPassword.classList.add("invalid");
     } else {
         passwordCheckPara.classList.remove("show");
         passwordCheckPara.classList.add("hidden");
         formButton.classList.remove("disabled");
         formButton.disabled = false;
+
+        password.classList.remove("invalid");
+        confirmPassword.classList.remove("invalid");
     }
 });
-
-
-/* formButton.addEventListener("click", () => {
-    if(password.value !== confirmPassword.value) {
-        passwordCheckPara.classList.remove("hidden");
-        passwordCheckPara.classList.add("show");
-    } else {
-        passwordCheckPara.classList.remove("show");
-        passwordCheckPara.classList.add("hidden");
-    }
-}); */
